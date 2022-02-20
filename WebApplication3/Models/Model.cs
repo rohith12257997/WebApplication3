4f131d187941f83ac0e3e070edb8611da95ac4f6
rohith12257997 <A00245384@mycambrian.ca>
2022-02-20 02:16:43 -0500

    Created layout

---
+++ WebApplication3/Models/Model.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class Model
+    {
+        public string NumberInput1 { get; set; }
+
+        public string NumberInput2 { get; set; }
+    }
+}
--- WebApplication3/Pages/Intro.cshtml.cs
+++ WebApplication3/Pages/Intro.cshtml.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using WebApplication3.Models;
 
 namespace WebApplication3.Pages
 {
@@ -15,6 +15,16 @@ namespace WebApplication3.Pages
         public void OnGet()
         {
         }
-       
+        public void OnPostSubmit(Model numbers)
+        {
+            if (numbers.NumberInput1 != null && numbers.NumberInput2 != null)
+            {
+                this.Output = string.Format("First number: {0} \n Second number: {1}", numbers.NumberInput1, numbers.NumberInput2);
+            }
+            else
+            {
+                this.Output = string.Empty;
+            }
+        }
     }
 } 
